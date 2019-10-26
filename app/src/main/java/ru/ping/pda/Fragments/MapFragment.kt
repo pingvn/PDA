dc6190ccee00fab807e7f6c403ce07f1259069ca
Kirill <pingvn@gmail.com>
2019-10-26 09:28:07 +0300

    комментарии и попытка разобраться с точностью координат

--- app/src/main/java/ru/ping/pda/Fragments/MapFragment.kt
+++ app/src/main/java/ru/ping/pda/Fragments/MapFragment.kt
@@ -1,8 +1,13 @@
+/*
+фрагмент отображения карты OpenStreetMap
+предположительное использоване : отоброжение карты местности по полученым координатам gps c устройства,
+отоброжение курсора на карте по полученным данным.
+установка областей получнных от других источников, отрисовка трека. расположение объектов на карте создание ответных действий при приближении к заданному объекту.
+сохранение геоданных на устройстве(трека)
+ */
 package ru.ping.pda.Fragments
 
 import android.content.Context
-import android.content.res.Resources
-import android.content.res.Resources.Theme
 import android.location.Location
 import android.net.Uri
 import android.os.Build
@@ -26,7 +31,7 @@ private lateinit var fusedLocationClient: FusedLocationProviderClient
 private lateinit var locationCallback: LocationCallback
 
 class MapFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+    // это сгенерированный код
     private var param1: String? = null
     private var param2: String? = null
     private var listener: OnFragmentInteractionListener? = null
@@ -48,27 +53,36 @@ class MapFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         val mView = inflater.inflate(R.layout.fragment_map, container, false)
+        //------------------------------------------------------------------------------------------
         val mMap : MapView = mView.findViewById(R.id.mapview)
         mMap.setTileSource(TileSourceFactory.MAPNIK)
+        //------------------------------------------------------------------------------------------
         val mapController : IMapController = mMap.controller //
         mapController.setZoom(16.5) // установка велечины зума при первоначальном запуске карты
+        //------------------------------------------------------------------------------------------
         mMap.setMultiTouchControls(true) // управление зумом двумя пальцами
-        var marker: Marker = Marker(mMap) // маркер точка на карте
+        //------------------------------------------------------------------------------------------
+        var marker: Marker = Marker(mMap) // маркер точки на карте
+        //изменение иконк маркера-------------------------------------------------------------------
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             marker.icon=resources.getDrawable(R.drawable.ic_position_point,null)
         }
+        //------------------------------------------------------------------------------------------
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(mView.context)
         //получение последних gps координат
+        //и установка маркера на карту
+        //отцентровка карты по маркеру
         fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
             if (location != null) {
-                marker.position= GeoPoint(location.latitude,location.longitude)
-                mMap.overlays.add(marker)
-                mapController.setCenter(GeoPoint(location.latitude,location.longitude))
+                marker.position= GeoPoint(location.latitude,location.longitude)//получение последних gps данных, передача данных маркеру
+                mMap.overlays.add(marker)//установка маркера
+                mapController.setCenter(GeoPoint(location.latitude,location.longitude))//центровка карты по маркеру
             }
         }
 
-
+        //изменение позиции маркера на карте после получения новых данных gps
         locationCallback = object : LocationCallback(){
             override fun onLocationResult(locationsResult: LocationResult?) {
                 super.onLocationResult(locationsResult)
@@ -86,22 +100,16 @@ class MapFragment : Fragment() {
 
         return mView
     }
-
+    //функция запуска отслеживания gps позиции
     fun startLocationUpdate(){
-        val request = LocationRequest.create().apply {
-            interval =2000
-            fastestInterval = 1000
-            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        }
-        fusedLocationClient.requestLocationUpdates(request, locationCallback, Looper.getMainLooper())
+        val request_GPS = LocationRequest.create()
+        request_GPS.setInterval(50)
+        request_GPS.setFastestInterval(25)
+        fusedLocationClient.requestLocationUpdates(request_GPS, locationCallback, Looper.getMainLooper())
     }
 
 
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)

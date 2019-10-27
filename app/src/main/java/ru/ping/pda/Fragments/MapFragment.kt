a1d6fd0240cdfac6688874c2de425b38ad217519
Kirill <pingvn@gmail.com>
2019-10-27 15:50:53 +0300

    попытки слушать gps

--- app/src/main/java/ru/ping/pda/Fragments/MapFragment.kt
+++ app/src/main/java/ru/ping/pda/Fragments/MapFragment.kt
@@ -1,11 +1,12 @@
 package ru.ping.pda.Fragments
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.location.Location
+import android.location.LocationManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -16,8 +17,12 @@ import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
-import ru.ping.pda.Data_Model.GPS_Data_Track
+import ru.ping.pda.GPS.GPS_Location
 import ru.ping.pda.R
+import java.util.*
+import kotlin.concurrent.schedule
+
+
 /*
 фрагмент отображения карты OpenStreetMap
 предположительное использоване : отоброжение карты местности по полученым координатам gps c устройства,
@@ -27,12 +32,19 @@ import ru.ping.pda.R
  */
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-private lateinit var fusedLocationClient: FusedLocationProviderClient
-private lateinit var locationCallback: LocationCallback
+//private lateinit var fusedLocationClient: FusedLocationProviderClient
+//private lateinit var locationCallback: LocationCallback
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), LocationListener {
+
+    override fun onLocationChanged(location: Location?) {
+        if (location != null){
+            myPoint = location
+        }
+    }
+
     // это сгенерированный код
-
+    var myPoint: Location? = null
     private var param1: String? = null
     private var param2: String? = null
     private var listener: OnFragmentInteractionListener? = null
@@ -47,19 +59,21 @@ class MapFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
-        startLocationUpdate()
+        // startLocationUpdate()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         val mView = inflater.inflate(R.layout.fragment_map, container, false)
+        septupLocationListener(mView.context)
         //------------------------------------------------------------------------------------------
-        val mMap : MapView = mView.findViewById(R.id.mapview)
+        val mMap: MapView = mView.findViewById(R.id.mapview)
         mMap.setTileSource(TileSourceFactory.MAPNIK)
         //------------------------------------------------------------------------------------------
-        val mapController : IMapController = mMap.controller //
+        val mapController: IMapController = mMap.controller
         mapController.setZoom(16.5) // установка велечины зума при первоначальном запуске карты
         //------------------------------------------------------------------------------------------
         mMap.setMultiTouchControls(true) // управление зумом двумя пальцами
@@ -67,49 +81,79 @@ class MapFragment : Fragment() {
         var marker: Marker = Marker(mMap) // маркер точки на карте
         //изменение иконк маркера-------------------------------------------------------------------
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            marker.icon=resources.getDrawable(R.drawable.ic_position_point,null)
+            marker.icon = resources.getDrawable(R.drawable.ic_position_point, null)
         }
+        //  gps.myPoint
         //------------------------------------------------------------------------------------------
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(mView.context)
+        //fusedLocationClient = LocationServices.getFusedLocationProviderClient(mView.context)
         //получение последних gps координат
         //и установка маркера на карту
         //отцентровка карты по маркеру
-        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-            if (location != null) {
-                marker.position= GeoPoint(location.latitude,location.longitude)//получение последних gps данных, передача данных маркеру
-                mMap.overlays.add(marker)//установка маркера
-                mapController.setCenter(GeoPoint(location.latitude,location.longitude))//центровка карты по маркеру
-            }
+        /*
+              fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                  if (location != null) {
+                      marker.position= GeoPoint(location.latitude,location.longitude)//получение последних gps данных, передача данных маркеру
+                      mMap.overlays.add(marker)//установка маркера
+                      mapController.setCenter(GeoPoint(location.latitude,location.longitude))//центровка карты по маркеру
+                  }
+              }
+
+              //изменение позиции маркера на карте после получения новых данных gps
+              locationCallback = object : LocationCallback(){
+                  override fun onLocationResult(locationsResult: LocationResult?) {
+                      super.onLocationResult(locationsResult)
+                      if (locationsResult != null) {
+                          for(location in locationsResult.locations){
+                              marker.position= GeoPoint(location.latitude,location.longitude)
+                              mMap.overlays.add(marker)
+                              //центровать карту по маркеру
+                              //mapController.setCenter(GeoPoint(location.latitude,location.longitude))
+                          }
+
+                      }
+                  }
+              }
+
+          */
+        Timer().schedule(1000){
+            myPoint?.let { setPoint(mMap,marker,it) }
         }
-
-        //изменение позиции маркера на карте после получения новых данных gps
-        locationCallback = object : LocationCallback(){
-            override fun onLocationResult(locationsResult: LocationResult?) {
-                super.onLocationResult(locationsResult)
-                if (locationsResult != null) {
-                    for(location in locationsResult.locations){
-                        marker.position= GeoPoint(location.latitude,location.longitude)
-                        mMap.overlays.add(marker)
-                        //центровать карту по маркеру
-                        //mapController.setCenter(GeoPoint(location.latitude,location.longitude))
-                    }
-
-                }
-            }
-        }
-
+        myPoint?.let { zoomPoint(mapController, it) }
+        myPoint?.let { setPoint(mMap,marker,it) }
         return mView
     }
+
+    /*
     //функция запуска отслеживания gps позиции
     fun startLocationUpdate(){
         val request_GPS = LocationRequest.create()
         request_GPS.setInterval(50)
         request_GPS.setFastestInterval(25)
         fusedLocationClient.requestLocationUpdates(request_GPS, locationCallback, Looper.getMainLooper())
+    }*/
+    fun zoomPoint(mController: IMapController, location: Location) {
+        mController.setCenter(GeoPoint(location.latitude, location.longitude))
     }
 
+    fun setPoint(map: MapView, marker: Marker, location: Location) {
+        marker.position = GeoPoint(location.latitude, location.longitude)
+        map.overlays.add(marker)
 
+    }
 
+    @SuppressLint("MissingPermission")
+    fun septupLocationListener(context: Context) {
+        var locationManager: LocationManager =
+            context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        var locationListener: GPS_Location = GPS_Location()
+        locationManager.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            0,
+            1f,
+            locationListener
+        )
+        myPoint = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
--- app/src/main/java/ru/ping/pda/MainActivity.kt
+++ app/src/main/java/ru/ping/pda/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.ping.pda
 
+
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,11 +10,12 @@ import android.view.WindowManager
 import android.widget.Button
 import ru.ping.pda.Fragments.MapFragment
 
-class MainActivity : AppCompatActivity() , MapFragment.OnFragmentInteractionListener,View.OnClickListener{
-    lateinit var menu_Button:Button
-    lateinit var record_Button:Button
-    lateinit var mail_Button:Button
-    lateinit var position_Button:Button
+class MainActivity : AppCompatActivity(), MapFragment.OnFragmentInteractionListener,
+    View.OnClickListener {
+    lateinit var menu_Button: Button
+    lateinit var record_Button: Button
+    lateinit var mail_Button: Button
+    lateinit var position_Button: Button
 
     val FRAGMENT_MAP_NEW = "mew"
     val FRAGMENT_MAP_REFRESH = "refresh"
@@ -25,23 +27,32 @@ class MainActivity : AppCompatActivity() , MapFragment.OnFragmentInteractionList
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        org.osmdroid.config.Configuration.getInstance().load(applicationContext,PreferenceManager.getDefaultSharedPreferences(applicationContext))
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.activity_main)
+
         initElements()
         runFragment(FRAGMENT_MAP_NEW)
     }
+
     override fun onClick(v: View?) {
-       when(v?.id){
-           R.id.id_button_PDA_MENU ->{
-           }
-           R.id.id_button_PDA_POSITION->{
-               runFragment(FRAGMENT_MAP_REFRESH)
-           }
-       }
+        when (v?.id) {
+            R.id.id_button_PDA_MENU -> {
+            }
+            R.id.id_button_PDA_POSITION -> {
+                runFragment(FRAGMENT_MAP_REFRESH)
+            }
+        }
     }
+
     //-------------------------------------------------------------------------------------------------------------
-    fun initElements(){
+    fun initElements() {
+        org.osmdroid.config.Configuration.getInstance().load(
+            applicationContext,
+            PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        )
         //инициализация кнопок
         menu_Button = findViewById(R.id.id_button_PDA_MENU)
         record_Button = findViewById(R.id.id_button_PDA_QUEST)
@@ -53,10 +64,17 @@ class MainActivity : AppCompatActivity() , MapFragment.OnFragmentInteractionList
         mail_Button.setOnClickListener(this)
         position_Button.setOnClickListener(this)
     }
-    fun runFragment(run_parametr:String){
-        when(run_parametr){
-            FRAGMENT_MAP_NEW->{  supportFragmentManager.beginTransaction().add(R.id.id_Conteiner_Fragment,MapFragment()).commit()}
-            FRAGMENT_MAP_REFRESH->{  supportFragmentManager.beginTransaction().replace(R.id.id_Conteiner_Fragment,MapFragment()).commit()}
+
+    fun runFragment(run_parametr: String) {
+        when (run_parametr) {
+            FRAGMENT_MAP_NEW -> {
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.id_Conteiner_Fragment, MapFragment()).commit()
+            }
+            FRAGMENT_MAP_REFRESH -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.id_Conteiner_Fragment, MapFragment()).commit()
+            }
         }
 
     }

47f2efff3765d6038aa1d3aba468d513ba491125
Kirill <pingvn@gmail.com>
2019-10-28 16:38:56 +0300

    gps is done

--- app/src/main/java/ru/ping/pda/Fragments/MapFragment.kt
+++ app/src/main/java/ru/ping/pda/Fragments/MapFragment.kt
@@ -11,14 +11,15 @@ import android.view.View
 import android.view.ViewGroup
 import com.google.android.gms.location.*
 import org.osmdroid.api.IMapController
+import org.osmdroid.api.IMapView
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
 import ru.ping.pda.R
 import java.util.*
 import kotlin.concurrent.schedule
-import kotlin.concurrent.timer
-import kotlin.concurrent.timerTask
+
 
 /*
 фрагмент отображения карты OpenStreetMap
@@ -33,16 +34,17 @@ private const val ARG_PARAM2 = "param2"
 private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 private lateinit var locationRequest: LocationRequest
 private lateinit var locationCallback: LocationCallback
-private lateinit var location:Location
 
 
-class MapFragment : Fragment(){
+
+class MapFragment : Fragment() {
 
 
     // это сгенерированный код
     private var param1: String? = null
     private var param2: String? = null
     private var listener: OnFragmentInteractionListener? = null
+    var fistrun: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,14 +52,13 @@ class MapFragment : Fragment(){
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-        getLocationUpdate()
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val mView = inflater.inflate(R.layout.fragment_map, container, false)
         //------------------------------------------------------------------------------------------
         val mMap: MapView = mView.findViewById(R.id.mapview)
@@ -73,42 +74,47 @@ class MapFragment : Fragment(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             marker.icon = resources.getDrawable(R.drawable.ic_position_point, null)
         }
-
-        Timer().schedule(1000){
-            
-        }
-
-
+        getLocationUpdate(mView.context,mMap,marker,mapController)
         return mView
     }
 
-    private fun getLocationUpdate(){
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context!!)
+    private fun getLocationUpdate(context: Context,map :MapView, marker:Marker,mapController:IMapController) {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
         locationRequest = LocationRequest()
         locationRequest.interval = 500
         locationRequest.fastestInterval = 500
         locationRequest.smallestDisplacement = 10f
         locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        locationCallback = object : LocationCallback(){
+        locationCallback = object : LocationCallback() {
             override fun onLocationResult(p0: LocationResult?) {
                 super.onLocationResult(p0)
                 if (p0 != null) {
-                    if (p0.locations.isNotEmpty()) location = p0.lastLocation
+                    marker.position = GeoPoint(p0.lastLocation.latitude,p0.lastLocation.longitude)
+                    map.overlays.add(marker)
+                    map.invalidate()
+                   // map.refreshDrawableState()
+                    if (fistrun){
+                        fistrun = false
+                        mapController.setCenter(GeoPoint(p0.lastLocation.latitude,p0.lastLocation.longitude))
+                    }
+
                 }
             }
         }
     }
 
-    private fun startLocationUpdate(){
+    private fun startLocationUpdate() {
         fusedLocationProviderClient.requestLocationUpdates(
             locationRequest,
             locationCallback,
             null
         )
     }
-    private fun stopLocationUpdate(){
+
+    private fun stopLocationUpdate() {
         fusedLocationProviderClient.removeLocationUpdates(locationCallback)
     }
+
 
     override fun onResume() {
         super.onResume()
